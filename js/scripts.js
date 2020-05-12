3492e5dcbe4425c07e475fb96c7934f87d9d762b
moallafeiz <feiz.moalla@etudiant-fst.utm.tn>
2020-05-12 03:21:52 +0100

    v1.0

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $('#mycarousel').carousel({ interval: 500 });
+    /*
+    $('#carousel-pause').click(function () {
+        $('#mycarousel').carousel('pause');
+    });
+   
+    $('#carousel-play').click(function () {
+        $('#mycarousel').carousel('cycle');
+    });
+    */
+    $('#carouselButton').click(function () {
+        if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }
+        else if ($('#carouselButton').children('span').hasClass('fa-play')) {
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        };
+
+    });
+
+    $('#reserveTableButton').click(function () {
+        $('#reserveTableModal').modal('toggle');
+
+    });
+
+    $('#loginButton').click(function () {
+        $('#loginModal').modal('toggle');
+    });
+
+});
